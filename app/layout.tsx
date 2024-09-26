62533ce4004c5c851378fa19c990dd9bbbb082c7
dbahiense <douglas@rohling.com.br>
2024-09-26 20:35:51 -0300

    refactor: change layout to a simpler one

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import "@/app/styles/globals.css";
+import { geistSans, geistMono } from "@/app/fonts";
+import { Toaster } from "@/components/ui";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -29,6 +19,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Toaster />
       </body>
     </html>
   );
