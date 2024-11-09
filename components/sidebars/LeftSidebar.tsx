a69683cba9703044fd146f39cf42a9960a42698a
dbahiense <douglas@rohling.com.br>
2024-11-09 11:42:14 -0300

    feature: add sidebars

---
+++ components/sidebars/LeftSidebar.tsx
@@ -0,0 +1,9 @@
+const LeftSidebar = () => {
+  return (
+    <div className='border border-orange-500 flex flex-col p-4 w-full'>
+      LeftSidebar
+    </div>
+  )
+}
+
+export default LeftSidebar
---
+++ components/sidebars/RightSidebar.tsx
@@ -0,0 +1,9 @@
+const RightSidebar = () => {
+  return (
+    <div className='border border-purple-500 flex flex-col p-4 w-full'>
+      RightSidebar
+    </div>
+  )
+}
+
+export default RightSidebar
