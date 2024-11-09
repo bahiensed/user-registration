3ee3f9eaaf9236b305087e9c7ccedb6284b3ad56
dbahiense <douglas@rohling.com.br>
2024-11-09 11:41:04 -0300

    chore: modify standard layout and page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import UserForm from "@/components/forms/User";
 
 export default function Home() {
   return (
-    <UserForm />
+    <div className='border border-blue-500 flex flex-col w-full px-8 py-4'>
+      <UserForm />
+    </div>
   );
 }
